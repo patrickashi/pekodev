8fd6200aa4ef846e210f91c5771a1be26d43c7bc
Patrick Ashi <patrickpeko1@gmail.com>
2024-09-18 09:19:58 +0100

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 import pekodevnew from "../Assets/pekodevnew.png";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div className="  duration-200 text-black ">
+        <div className="duration-200 text-black ">
             <div className="flex gap-16 flex-col px-2 md:px-10 lg:px-16 py-10 md:flex-row bg-[#e9d5ff] justify-between pb-4">
                 <div>
                     <div className="flex align-middle hover:text-black ">
@@ -31,9 +35,39 @@ const Footer = () => {
                 <div>
                     <h1 className="font-bold mb-4">Social</h1>
                     <ul className="flex flex-col gap-2">
-                        <li>whatsapp</li>
-                        <li>Instagram</li>
-                        <li>Youtube</li>
+                        <a href="http://wa.me/2347038905049">
+                            <li>
+                                <div className="flex align-middle">
+                                    <FaWhatsapp className="mt-1 mr-1 text-emerald-700" />
+                                    Whatsapp
+                                </div>
+                            </li>
+                        </a>
+                        <a href="https://www.instagram.com/patrickpeko_">
+                            <li>
+                                <div className="flex">
+                                    <FaInstagram className="mt-1 mr-1 text-red-600"/>
+                                    Instagram
+                                </div>
+                            </li>
+                        </a>
+                        
+                        <a href="https://www.TikTok.com/@patrickpeko_">
+                            <li>
+                                <div className="flex">
+                                    <FaTiktok className="mt-1 mr-1"/>
+                                    Tiktok
+                                </div>
+                            </li>
+                        </a>
+                        <a href="https://www.youtube.com/@patrickpeko">
+                            <li>
+                                <div className="flex">
+                                    <FaYoutube className="mt-1 mr-1 text-red-600"/>
+                                    Youtube
+                                </div>
+                            </li>
+                        </a>
                     </ul>
                 </div>
             </div>
